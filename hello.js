698dbefdcf36abe2f8a819c53b39cddaf7b301ea
Dev Taha <tahayasseralshorafa@gmail.com>
2023-04-25 15:52:16 +0300

    commit {hello}

--- hello.js
+++ hello.js
@@ -1,4 +1,9 @@
 console.log('hello world!');
 const name = 'Taha Ahmed';
-console.log(`Wlcome in your App Mr@{name}`);
+console.log(`Wlcome in your App Mr @{name}`);
 
+function getName(name) {
+    console.log(`Hi Mr @{name}`);
+}
+
+getName('taha');
